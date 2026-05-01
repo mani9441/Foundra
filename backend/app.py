a12465752ada24baab5fa26006a31fe86d025d2c
manikantatechnologiespvtltd-dev <manikantatechnologiespvtltd@gmail.com>
2026-05-02 02:20:20 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -42,8 +42,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080",
         "http://localhost:5173",
-        "https://foundra-main.vercel.app"
-        "*"   # replace with frontend domain in prod
+        "https://foundra-main.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
